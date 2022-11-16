2b6a5e9e2494677060c6d37e2e4add8ea82b914a
anouar052 <anouar.alh@gmil.com>
2022-11-16 16:16:13 +0100

    add .env

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'floower-store.myshopify.com',
-    storefrontToken: '9fc0919a5df6bea1e6de28d3b9c00e57',
+    storeDomain: import.meta.env.PUBLIC_STORE_DOMAIN,
+    storefrontToken: import.meta.env.PUBLIC_STOREFRONT_TOKEN,
     // storefrontToken: '4c05ec4e786e07a745203b7f7af02bba',
     storefrontApiVersion: '2022-07',
   },
